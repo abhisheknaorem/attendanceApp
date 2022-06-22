f0f0144933bbdbb6d261104b426abf613185b907
abhisheknaorem <107980700+abhisheknaorem@users.noreply.github.com>
2022-06-22 12:09:16 +0530

    Add files via upload

---
+++ homescreen.dart
@@ -0,0 +1,170 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutterproject/calendarscreen.dart';
+import 'package:flutterproject/model/user.dart';
+import 'package:flutterproject/profilescreen.dart';
+import 'package:flutterproject/services/location_service.dart';
+import 'package:flutterproject/todayscreen.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  double screenHeight = 0;
+  double screenWidth = 0;
+
+  String id = '';
+
+  Color primary = const Color.fromARGB(253, 9, 46, 64);
+
+  int currentIndex = 1;
+
+  List<IconData> navigationIcons = [
+    FontAwesomeIcons.calendarDay,
+    FontAwesomeIcons.check,
+    FontAwesomeIcons.user,
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _startLocationService();
+    _getCredentials();
+    getId();
+  }
+
+  void _getCredentials() async {
+    DocumentSnapshot doc = await FirebaseFirestore.instance
+        .collection("employee")
+        .doc(User.id)
+        .get();
+    setState(() {
+      User.canEdit = doc['canEdit'];
+      User.firstName = doc['firstName'];
+      User.lastName = doc['lastName'];
+      User.birthDate = doc['birthDate'];
+      User.address = doc['address'];
+    });
+  }
+
+  void _startLocationService() async {
+    LocationService().initialize();
+
+    LocationService().getLongitude().then((value) {
+      setState(() {
+        User.long = value!;
+      });
+
+      LocationService().getLatitude().then((value) {
+        setState(() {
+          User.lat = value!;
+        });
+      });
+    });
+  }
+
+  void getId() async {
+    QuerySnapshot snap = await FirebaseFirestore.instance
+        .collection("employee")
+        .where('id', isEqualTo: User.employeeId)
+        .get();
+
+    setState(() {
+      User.id = snap.docs[0].id;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    screenHeight = MediaQuery.of(context).size.height;
+    screenWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      body: IndexedStack(
+        index: currentIndex,
+        children: [
+          new CalendarScreen(),
+          new TodayScreen(),
+          new ProfileScreen(),
+        ],
+      ),
+      bottomNavigationBar: Container(
+        height: 70,
+        margin: const EdgeInsets.only(
+          left: 12,
+          right: 12,
+          bottom: 24,
+        ),
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.all(Radius.circular(40)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black26,
+              blurRadius: 10,
+              offset: Offset(2, 2),
+            ),
+          ],
+        ),
+        child: ClipRRect(
+          borderRadius: const BorderRadius.all(Radius.circular(40)),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              for (int i = 0; i < navigationIcons.length; i++) ...<Expanded>{
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        currentIndex = i;
+                      });
+                    },
+                    child: Container(
+                      height: screenHeight,
+                      width: screenWidth,
+                      color: Colors.white,
+                      child: Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              navigationIcons[i],
+                              color: i == currentIndex
+                                  ? Color.fromARGB(255, 33, 4, 219)
+                                  : primary,
+                              size: i == currentIndex ? 30 : 26,
+                            ),
+                            i == currentIndex
+                                ? Container(
+                                    margin: const EdgeInsets.only(top: 6),
+                                    height: 3,
+                                    width: 22,
+                                    decoration: BoxDecoration(
+                                      borderRadius: const BorderRadius.all(
+                                          Radius.circular(40)),
+                                      color: primary,
+                                    ),
+                                  )
+                                : const SizedBox(),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              }
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
